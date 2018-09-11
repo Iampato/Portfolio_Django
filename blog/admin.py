ebda6ebeff982771bcad11b66f92686f67bb00a3
Iampato <858wpwaweru@gmail.com>
2018-09-11 18:23:16 +0300

    added admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
 from .models import Post,subscriber
+
+class subscriberAdmin(admin.ModelAdmin):
+	list_display = ('Name', 'Email', 'sent_on')
+	list_filter = ( 'active','sent_on', 'updated')
+	search_fields = ('Name', 'Email')
+admin.site.register(subscriber, subscriberAdmin)
