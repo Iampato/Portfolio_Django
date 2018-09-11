e965b5ab94e40e8a94015d66f53624630fad8129
Iampato <858wpwaweru@gmail.com>
2018-09-11 18:07:21 +0300

    Added templates

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ def post_list_view(request):
         posts = paginator.page(paginator.num_pages)
 
        
-    return render(request,'blog/post/list.html',{'page': page,'posts': posts})
+    return render(request,'blog/list.html',{'page': page,'posts': posts})
 
 def post_detail_view(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
