e1d567efada6469d0bdbed04ea79d9d89651febe
Iampato <858wpwaweru@gmail.com>
2018-09-11 18:20:37 +0300

    Added the  postfeed class

--- blog/feeds.py
+++ blog/feeds.py
@@ -11,6 +11,7 @@ class PostsFeed(Feed):
     def items(self):
         return Post.published.all()[:5]
     def item_title(self, item):
+        return item.title
 """
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
