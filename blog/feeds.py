5e86e8a2ff58a85bd20ca930c7f2c42920015572
Iampato <858wpwaweru@gmail.com>
2018-09-11 18:14:36 +0300

    import required modules

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,5 +1,6 @@
 from django.contrib.syndication.views  import Feed
 from django.template.defaultfilters import truncatewords
+from .models import Post
 """
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
