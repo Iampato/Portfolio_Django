d4d97f14ccdbd037b4405755fc3e0623cd6cc689
Iampato <858wpwaweru@gmail.com>
2018-09-11 18:11:20 +0300

    Added feeds to my blog

---
+++ blog/feeds.py
@@ -0,0 +1,17 @@
+"""
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from .models import Post
+from django.contrib.sitemaps.views import sitemap
+ 
+class PostsFeed(Feed):
+    title = 'Dekutshades Blog Feeds'
+    link = '/blog/'
+    description = 'Our latest Posts!'
+ 
+    def items(self):
+        return Post.published.all()[:5]
+ 
+    def item_title(self, item):
+        return item.title
+ """
