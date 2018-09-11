35978a619b00df2f510115f30bbad4f873f483d8
Iampato <858wpwaweru@gmail.com>
2018-09-11 18:28:10 +0300

    added patterns

--- personal/urls.py
+++ personal/urls.py
@@ -1,1 +1,27 @@
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls.static import static
+from filebrowser.sites import site
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/filebrowser/',include(site.urls)),
+    url('', include('welcome.urls', namespace='welcome', app_name='welcome')),
+    url('blog/', include('blog.urls', namespace='blog', app_name='blog')),
+    url('event/', include('Event.urls', namespace='Event', app_name='Event')),
+    url('contact/', include('contact.urls', namespace='contact', app_name='contact')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+            name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^tinymce/', include('tinymce.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+else:
+	urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+	urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
+
 
