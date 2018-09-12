73d2f4af23296c70e638a07942a854e2e9d61015
Iampato <858wpwaweru@gmail.com>
2018-09-12 21:57:24 +0300

    added forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,14 @@ from django import forms
 class subscribeForm(forms.ModelForm):
 	class Meta:
 		model = subscriber
+		
+		Name = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={
+            'class' : 'form-control',
+            'placeholder' : 'Full Name' } ))
+		Email = forms.EmailField(required=True,  widget=forms.EmailInput(
+        attrs={
+            'class' : 'form-control',
+            'placeholder' : 'Email' } ))
+
 		fields = ('Name','Email')
