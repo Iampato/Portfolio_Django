4607fa678e7b12e90013b8531762515766ca85a2
Iampato <858wpwaweru@gmail.com>
2018-09-12 21:38:28 +0300

    send email function

--- blog/forms.py
+++ blog/forms.py
@@ -1,26 +1,7 @@
-from django.forms import ModelForm
+from .models import subscriber
 from django import forms
-from .models import subscribe
 
-# our new form
-class ContactForm(forms.Form):
-    Name = forms.CharField(required=True, widget=forms.TextInput(
-        attrs={
-            'class' : 'form-control',
-            'placeholder' : 'Full Name'
-        }
-    ))
-    Email = forms.EmailField(required=True,  widget=forms.EmailInput(
-        attrs={
-            'class' : 'form-control',
-            'placeholder' : 'Email'
-        }
-    ))
-    
 class subscribeForm(forms.ModelForm):
-    class Meta:
-        model = subscribe
-        fields = ('Name', 'Email')
-        
-
-
+	class Meta:
+		model = subscriber
+		fields = ('Name','Email')
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from django.shortcuts import render, get_object_or_404,redirect
 from django.conf import settings
-from .models import Post
+from .models import Post,subscriber
+
+from django.core.mail import send_mail
+from django.contrib import messages
+from .forms import  subscribeForm
 
 
 def post_list_view(request):
+
     object_list = Post.published.all()    
     paginator = Paginator(object_list, 4) # 4 posts in each page   
     page = request.GET.get('page')
@@ -15,8 +20,35 @@ def post_list_view(request):
     except EmptyPage:        # If page is out of range deliver last page of results        
         posts = paginator.page(paginator.num_pages)
 
-       
-    return render(request,'blog/list.html',{'page': page,'posts': posts})
+    
+    if request.method == 'POST':
+        form = subscribeForm(data=request.POST)
+
+        if form.is_valid():
+            message = form.save(commit=False)
+
+            if subscriber.objects.filter(Email = message.Email).exists():
+                messages.warning(request,  "Your Email already exists in our Databases")
+            else:
+                message.save()
+
+                Name = request.POST.get( 'Name', '')
+                Email = request.POST.get( 'Email', '') 
+                Subject = 'Welcome to Patrick Blog'
+                message = 'Dear %s \nWelcome to my blog, Thanks for Subscribing to our mail.\n By signing up you agree with our terms and Conditions where they apply. ' %(Name)
+                emailFrom =  settings.EMAIL_HOST_USER 
+                emailTo =  Email 
+                
+                
+                send_mail(Subject,message,emailFrom,[emailTo],fail_silently=False)
+                
+                messages.success(request,  "You Successfully subscribed")
+
+        return redirect('/blog')
+    else:
+        form = subscribeForm() 
+
+    return render(request,'blog/list.html',{'page': page,'form':form,'posts': posts})
 
 def post_detail_view(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
