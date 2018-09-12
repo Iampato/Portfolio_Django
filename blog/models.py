0ef3595de75b1086986a7190de2b88d750907b8c
Iampato <858wpwaweru@gmail.com>
2018-09-12 21:10:04 +0300

    added subscribe to our mail

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,19 @@ class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,self).get_queryset().filter(status='published')
 
+class subscriber(models.Model):
+    Name = models.CharField(max_length=80)
+    Email = models.EmailField()
+    sent_on = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('sent_on',)
+
+    def __str__(self):
+        return self.Email
+
 # Our Post Model
 class Post(models.Model):
     STATUS_CHOICES = (
